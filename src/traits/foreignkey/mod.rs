06e75b688d7bcf1db3376db2ff72820d45d5047c
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-10-06 10:58:19 -0400

    fix get_fk()

--- src/traits/foreignkey/mod.rs
+++ src/traits/foreignkey/mod.rs
@@ -14,9 +14,13 @@ pub trait ForeignKeyModel<T: PrimaryKeyModel, U: PrimaryKeyModel>: ForeignKey<T>
 }
 impl<S: PrimaryKeyModel, T: ForeignKey<S>, U: PrimaryKeyModel> ForeignKeyModel<S, U> for T {
     fn get_all_by_fk(db: &mut impl DbCtx, references: &U) -> Result<Vec<Self>, Error> {
-        let sql = format!(
-            "select {}.* from {} as {} where {}.{} = {};",
-            Self::ALIAS, Self::TABLE, Self::ALIAS, Self::ALIAS, Self::FOREIGN_KEY, Self::FOREIGN_KEY_PARAM
+        let sql = format!("
+            select {}.*
+            from {}.{} as {}
+            where {}.{} = {};",
+            Self::ALIAS,
+            Self::DB, Self::TABLE, Self::ALIAS,
+            Self::ALIAS, Self::FOREIGN_KEY, Self::FOREIGN_KEY_PARAM
         );
         let c = db.use_connection();
         let mut stmt = c.prepare(&sql)?;
@@ -25,9 +29,17 @@ impl<S: PrimaryKeyModel, T: ForeignKey<S>, U: PrimaryKeyModel> ForeignKeyModel<S
         })?.into_iter().collect();
     }
     fn get_fk(&self, db: &mut impl DbCtx) -> Result<U, Error> {
-        let sql = format!(
-            "select {}.* from {} as {} join {} as {} on {}.{} = {}.{} and {}.{} = {}",
-            Self::ALIAS, Self::TABLE, Self::ALIAS, U::TABLE, U::ALIAS, Self::ALIAS, Self::FOREIGN_KEY, U::ALIAS, U::PRIMARY_KEY, U::ALIAS, U::PRIMARY_KEY, Self::FOREIGN_KEY_PARAM
+        let sql = format!("
+            select {}.*
+            from {}.{} as {}
+            join {}.{} as {}
+            on {}.{} = {}.{}
+            and {}.{} = {}",
+            U::ALIAS,
+            Self::DB, Self::TABLE, Self::ALIAS,
+            U::DB, U::TABLE, U::ALIAS,
+            Self::ALIAS, Self::FOREIGN_KEY, U::ALIAS, U::PRIMARY_KEY,
+            U::ALIAS, U::PRIMARY_KEY, Self::FOREIGN_KEY_PARAM
         );
         let c = db.use_connection();
         let mut stmt = c.prepare(&sql)?;
