9e4d6648ed5e9d062c37f6de9062a6cb267ecd73
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-10-18 15:18:07 -0400

    implement foreign key for referenced struct

--- src/traits/foreignkey/mod.rs
+++ src/traits/foreignkey/mod.rs
@@ -8,6 +8,13 @@ pub trait ForeignKey<U: PrimaryKeyModel>: PrimaryKeyModel {
     const FOREIGN_KEY_PARAM: &'static str;
     fn get_fk_value(&self) -> i64;
 }
+impl<T: PrimaryKeyModel, U: ForeignKey<T>> ForeignKey<U> for T {
+    const FOREIGN_KEY: &'static str = T::PRIMARY_KEY;
+    const FOREIGN_KEY_PARAM: &'static str = U::FOREIGN_KEY_PARAM;
+    fn get_fk_value(&self) -> i64 {
+        self.get_id()
+    }
+}
 pub trait ForeignKeyModel<T: PrimaryKeyModel, U: PrimaryKeyModel>: ForeignKey<T> {
     fn get_all_by_fk(db: &mut impl DbCtx, references: &U) -> Result<Vec<Self>, Error>;
     fn get_fk(&self, db: &mut impl DbCtx) -> Result<U, Error>;
