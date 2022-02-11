b148629a5f0b5e031293fa9c6edead50623f7817
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2022-02-11 10:54:51 -0500

    implement all DbContext functions through DbCtx

--- src/traits/dbctx/mod.rs
+++ src/traits/dbctx/mod.rs
@@ -2,4 +2,7 @@ use rusqlite::Connection;
 pub trait DbCtx: Sized {
     fn init() -> Self;
     fn use_connection(&mut self) -> &mut Connection;
+    fn attach_temp_dbs(&mut self);
+    fn attach_dbs(&mut self);
+    fn delete_db_files(&mut self) -> Result<(), String>;
 }
